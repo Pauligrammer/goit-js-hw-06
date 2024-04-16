5f83acd98062104e4fe935ed99864e255deff5fc
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-04-16 12:21:32 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputName = document.getElementById('name-input');
+const outputName = document.getElementById('name-output');
+
+inputName.addEventListener('input', () => {
+    if (!inputName.value) {
+        outputName.textContent = 'Anonymous';
+    }
+    else {
+        outputName.textContent = inputName.value;
+    }
+});
