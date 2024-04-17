3b1934a898d9dd892e4d6581df529b3457011409
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-04-17 14:09:51 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const login = form.elements.login.value;
+    const password = form.elements.password.value;
+
+    if (login === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+    }
+    
+    console.log(`Login: ${login}, Password: ${password}`);
+    form.reset();
+});
