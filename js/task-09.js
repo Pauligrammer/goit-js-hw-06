08bedaad5c86b6d45d4c438ebf7da3e839e6cb86
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-04-17 15:26:34 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  color.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;  
+});
